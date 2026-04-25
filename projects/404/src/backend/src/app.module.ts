f63085e8ae47c7a352f36e49e3cd0af5afad5de0
Rohan Poudel <yitsmerohan@gmail.com>
2026-04-25 23:21:44 +0545

    feat: added medicine service as well its controller - medicine crud completed

--- projects/404/src/backend/src/app.module.ts
+++ projects/404/src/backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MedicineModule } from './medicine/medicine.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, MedicineModule],
   controllers: [AppController],
   providers: [AppService],
 })
